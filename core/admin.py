8303cb7b799b2f69aa581e8608053d8d3ab2b07b
Chao Zhang <chao.zh@gmail.com>
2025-08-27 13:27:42 -0400

    add models for ADC, COLDATA

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FEMB, FE
+from .models import FEMB, FE, ADC, COLDATA
 
 
 class FEMBAdmin(admin.ModelAdmin):
@@ -10,7 +10,21 @@ class FEAdmin(admin.ModelAdmin):
     list_display = ("serial_number", "status", "tray_id", "last_update", "femb")
 
 
+class ADCAdmin(admin.ModelAdmin):
+    list_display = ("serial_number", "status", "tray_id", "last_update", "femb")
+
+
+class COLDATAAdmin(admin.ModelAdmin):
+    list_display = ("serial_number", "status", "tray_id", "last_update", "femb")
+
+
 # admin.site.unregister(FEMB)
 # admin.site.unregister(FE)
 admin.site.register(FEMB, FEMBAdmin)
 admin.site.register(FE, FEAdmin)
+admin.site.register(ADC, ADCAdmin)
+admin.site.register(COLDATA, COLDATAAdmin)
+
+admin.site.site_header = "CETs Admin"
+admin.site.site_title = "CETs Admin Portal"
+admin.site.index_title = "Welcome to CETs Admin Portal"
--- core/models.py
+++ core/models.py
@@ -4,20 +4,25 @@ import os
 
 
 class FEMB(models.Model):
-    serial_number = models.CharField(max_length=50, unique=True)
-    status = models.CharField(max_length=20, default="unknown")
+    version = models.CharField(max_length=20, default="IO-1865-1K")
+    serial_number = models.CharField(max_length=10)
+    status = models.CharField(max_length=20, default="testing")
     last_update = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = [["version", "serial_number"]]
+
     def __str__(self):
-        return self.serial_number
+        return f"FEMB: {self.version}/{self.serial_number}"
 
 
 class FE(models.Model):
     serial_number = models.CharField(max_length=20, unique=True)
-    status = models.CharField(max_length=20, default="unknown")
+    status = models.CharField(max_length=20, default="testing")
     tray_id = models.CharField(max_length=20)
     last_update = models.DateTimeField(auto_now=True)
     femb = models.ForeignKey(FEMB, on_delete=models.CASCADE, null=True, blank=True)
+    femb_pos = models.CharField(max_length=2, null=True, blank=True)  # F1-4, B1-4
 
     def rts(self):
         rts_dir = config("RTS_DIR")
@@ -30,28 +35,54 @@ class FE(models.Model):
 
         parsed_files = []
         for filename in filelist:
-            if filename.startswith(self.serial_number) and filename.endswith('.csv'):
+            if filename.startswith(self.serial_number) and filename.endswith(".csv"):
                 try:
                     # Strip serial number and extension
                     parts_str = filename[len(self.serial_number) + 1 : -4]
-                    parts = parts_str.split('_')
+                    parts = parts_str.split("_")
                     if len(parts) == 4:
-                        parsed_files.append({
-                            'filename': filename,
-                            'serial_number': self.serial_number,
-                            'timestamp': parts[0],
-                            'tray': parts[1],
-                            'socket': parts[2],
-                            'temperature': parts[3],
-                        })
+                        parsed_files.append(
+                            {
+                                "filename": filename,
+                                "serial_number": self.serial_number,
+                                "timestamp": parts[0],
+                                "tray": parts[1],
+                                "socket": parts[2],
+                                "temperature": parts[3],
+                            }
+                        )
                 except IndexError:
                     # Ignore files that don't match the expected format
                     pass
-        
-        parsed_files.sort(key=lambda x: x['temperature'])
-        parsed_files.sort(key=lambda x: x['timestamp'], reverse=True)
-        
+
+        parsed_files.sort(key=lambda x: x["temperature"])
+        parsed_files.sort(key=lambda x: x["timestamp"], reverse=True)
+
         return parsed_files
 
     def __str__(self):
-        return self.serial_number
+        return f"LArASIC: {self.serial_number}"
+
+
+class ADC(models.Model):
+    serial_number = models.CharField(max_length=20, unique=True)
+    status = models.CharField(max_length=20, default="testing")
+    tray_id = models.CharField(max_length=20, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    femb = models.ForeignKey(FEMB, on_delete=models.CASCADE, null=True, blank=True)
+    femb_pos = models.CharField(max_length=2, null=True, blank=True)  # F1-4, B1-4
+
+    def __str__(self):
+        return f"ColdADC: {self.serial_number}"
+
+
+class COLDATA(models.Model):
+    serial_number = models.CharField(max_length=20, unique=True)
+    status = models.CharField(max_length=20, default="testing")
+    tray_id = models.CharField(max_length=20, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    femb = models.ForeignKey(FEMB, on_delete=models.CASCADE, null=True, blank=True)
+    femb_pos = models.CharField(max_length=2, null=True, blank=True)  # F1-2
+
+    def __str__(self):
+        return f"COLDATA: {self.serial_number}"
